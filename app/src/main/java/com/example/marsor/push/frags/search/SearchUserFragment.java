2e8da69d315eb06fbda4c8849c768a70fb5bd44c
marsor <369135912@qq.com>
2017-07-03 14:11:12 +0800

    实现用户搜索功能

--- app/src/main/java/com/example/marsor/push/frags/search/SearchUserFragment.java
+++ app/src/main/java/com/example/marsor/push/frags/search/SearchUserFragment.java
@@ -1,7 +1,17 @@
 package com.example.marsor.push.frags.search;
 
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 import com.example.common.app.PresenterFragment;
+import com.example.common.widget.EmptyView;
+import com.example.common.widget.PortraitView;
+import com.example.common.widget.recycler.RecyclerAdapter;
 import com.example.factory.model.card.UserCard;
 import com.example.factory.presenter.search.SearchContract;
 import com.example.factory.presenter.search.SearchUserPresenter;
@@ -10,19 +20,56 @@ import com.example.marsor.push.activities.SearchActivity;
 
 import java.util.List;
 
+import butterknife.BindView;
+
 /**
  * 搜索人的界面实现
  */
-public class SearchUserFragment extends PresenterFragment<SearchContract.Presenter> implements SearchActivity.SearchFragment,SearchContract.UserView{
+public class SearchUserFragment extends PresenterFragment<SearchContract.Presenter> implements SearchActivity.SearchFragment, SearchContract.UserView {
 
+    @BindView(R.id.empty)
+    EmptyView mEmptyView;
+
+    @BindView(R.id.recycler)
+    RecyclerView mRecycler;
+
+    private RecyclerAdapter<UserCard> mAdapter;
 
     public SearchUserFragment() {
-        // Required empty public constructor
     }
 
     @Override
     protected int getContentLayoutId() {
         return R.layout.fragment_search_user;
+    }
+
+    @Override
+    protected void initWidget(View root) {
+        super.initWidget(root);
+        //初始化Recycler
+        mRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecycler.setAdapter(mAdapter = new RecyclerAdapter<UserCard>() {
+            @Override
+            protected int getItemViewType(int position, UserCard userCard) {
+                //返回cell布局的id
+                return R.layout.cell_search_list;
+            }
+
+            @Override
+            protected ViewHolder<UserCard> onCreateViewHolder(View root, int viewType) {
+                return new SearchUserFragment.ViewHolder(root);
+            }
+        });
+        //初始化占位布局
+        mEmptyView.bind(mRecycler);
+        setPlaceHolderView(mEmptyView);
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+        //发起首次搜索
+        search("");
     }
 
     @Override
@@ -34,11 +81,42 @@ public class SearchUserFragment extends PresenterFragment<SearchContract.Present
     @Override
     public void onSearchDone(List<UserCard> userCards) {
         //数据成功情况下返回数据
+        mAdapter.replace(userCards);
+        //如果有数据则显示ok 没有则显示空布局
+        mPlaceHolderView.triggerOkOrEmpty(mAdapter.getItemCount() > 0);
     }
 
     @Override
     protected SearchContract.Presenter initPresenter() {
         //初始化presenter
         return new SearchUserPresenter(this);
+    }
+
+    /**
+     * 每一个cell的布局操作
+     */
+    class ViewHolder extends RecyclerAdapter.ViewHolder<UserCard> {
+        @BindView(R.id.im_portrait)
+        PortraitView mPortraitView;
+
+        @BindView(R.id.txt_name)
+        TextView mName;
+
+        @BindView(R.id.im_follow)
+        ImageView mFollow;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        protected void onBind(UserCard userCard) {
+            Glide.with(SearchUserFragment.this)
+                    .load(userCard.getPortrait())
+                    .centerCrop()
+                    .into(mPortraitView);
+            mName.setText(userCard.getName());
+            mFollow.setEnabled(!userCard.isFollow());
+        }
     }
 }
--- factory/src/main/java/com/example/factory/net/RemoteService.java
+++ factory/src/main/java/com/example/factory/net/RemoteService.java
@@ -7,8 +7,11 @@ import com.example.factory.model.api.account.RegisterModel;
 import com.example.factory.model.api.user.UserUpdateModel;
 import com.example.factory.model.card.UserCard;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
@@ -53,4 +56,12 @@ public interface RemoteService {
      */
     @PUT("user")
     Call<RspModel<UserCard>> userUpdate(@Body UserUpdateModel model);
+
+    /**
+     * 用户搜索的接口
+     * @param name 用户名
+     * @return RspModel<List<UserCard>>
+     */
+    @GET("user/search/{name}")
+    Call<RspModel<List<UserCard>>> userSearch(@Path("name") String name);
 }
